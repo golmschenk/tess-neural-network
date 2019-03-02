5b57811a246f681e90c81649670ab160a7fedbf7
Greg Olmschenk <golmschenk@gmail.com>
2019-03-01 19:44:02 -0500

    Ignore FITS which are read as corrupt

--- flare_catalog_download.py
+++ flare_catalog_download.py
@@ -5,9 +5,7 @@ import warnings
 import numpy as np
 from lightkurve import search_targetpixelfile, LightkurveWarning
 
-
-data_directory = 'data'
-flare_catalog_path = os.path.join(data_directory, 'flare_catalog.csv')
+from flare_catalog_dataset import data_directory, catalog_path
 
 
 def get_count_stars_in_flare_catalog_with_short_cadence_data():
@@ -25,17 +23,25 @@ def get_count_stars_in_flare_catalog_with_short_cadence_data():
 def download_all_short_cadence_observations_for_targets_in_flare_catalog():
     """Downloads and saves all the short cadence observations for the stars which appear in the flare catalog."""
     flare_catalog = np.genfromtxt(catalog_path, delimiter=',', skip_header=1)
-    kepler_input_catalog_numbers = map(int, [target[0] for target in flare_catalog])
+    kepler_input_catalog_numbers = list(map(int, [target[0] for target in flare_catalog]))
+    observations_skipped = 0
+    targets_skipped = 0
     for kepler_input_catalog_number in kepler_input_catalog_numbers:
         with warnings.catch_warnings():
             warnings.filterwarnings("ignore", category=LightkurveWarning)  # Ignore warnings about empty downloads.
-            short_cadence_observations = search_targetpixelfile(kepler_input_catalog_number,
-                                                                cadence='short').download_all()
-        if short_cadence_observations is None:
-            continue
-        for observation_index, observation in enumerate(short_cadence_observations):
-            observation_file_name = f'{kepler_input_catalog_number}_{observation_index}.fits'
-            observation.to_fits(os.path.join(data_directory, observation_file_name), overwrite=True)
+            current_target_skipped = False
+            short_cadence_observations = search_targetpixelfile(kepler_input_catalog_number, cadence='short')
+            for observation_index, observation in enumerate(short_cadence_observations):
+                observation_file_name = f'{kepler_input_catalog_number}_{observation_index}.fits'
+                try:
+                    observation.download().to_fits(os.path.join(data_directory, observation_file_name), overwrite=True)
+                except OSError:
+                    observations_skipped += 1
+                    if not current_target_skipped:
+                        current_target_skipped = True
+                        targets_skipped += 1
+    print(f'{targets_skipped} targets skipped.')
+    print(f'{observations_skipped} observations skipped.')
 
 
 if __name__ == '__main__':
