d5d74009eea11f9df134a25f0540c62b444d00ac
Greg Olmschenk <golmschenk@gmail.com>
2019-03-08 16:41:55 -0500

    Update download to use lightcurves instead of TPFs and allow for a maximum number of observations

--- koi_dataset.py
+++ koi_dataset.py
@@ -2,10 +2,12 @@
 import os
 import warnings
 import pandas as pd
-from lightkurve import search_targetpixelfile, LightkurveWarning
+from lightkurve import search_lightcurvefile, LightkurveWarning
 
 data_directory = 'koi_data'
 catalog_path = os.path.join(data_directory, 'cumulative_2019.03.08_12.09.57.csv')
+positive_data_directory = os.path.join(data_directory, 'positive')
+negative_data_directory = os.path.join(data_directory, 'negative')
 
 
 def get_easy_positive_stars():
@@ -22,7 +24,7 @@ def print_short_cadence_observation_count_for_stars(star_list):
     count_of_stars_with_short_cadence = 0
     count_of_observations_with_short_cadence = 0
     for kepler_input_catalog_number in star_list:
-        short_cadence_observations = search_targetpixelfile(kepler_input_catalog_number, cadence='short')
+        short_cadence_observations = search_lightcurvefile(kepler_input_catalog_number, cadence='short')
         count_of_observations_with_short_cadence += len(short_cadence_observations)
         if len(short_cadence_observations) is not 0:
             count_of_stars_with_short_cadence += 1
@@ -30,28 +32,43 @@ def print_short_cadence_observation_count_for_stars(star_list):
     print(f'Observations: {count_of_observations_with_short_cadence}')
 
 
-def download_all_short_cadence_observations_for_star_list(star_list):
+def download_all_short_cadence_observations_for_star_list(star_list, directory, max_observations=None):
     """Downloads and saves all the short cadence observations for the stars which appear in the star list."""
     observations_skipped = 0
     targets_skipped = 0
+    observations_downloaded = 0
+    os.makedirs(positive_data_directory, exist_ok=True)
     for kepler_input_catalog_number in star_list:
         with warnings.catch_warnings():
             warnings.filterwarnings("ignore", category=LightkurveWarning)  # Ignore warnings about empty downloads.
             current_target_skipped = False
-            short_cadence_observations = search_targetpixelfile(kepler_input_catalog_number, cadence='short')
+            short_cadence_observations = search_lightcurvefile(kepler_input_catalog_number, cadence='short')
             for observation_index, observation in enumerate(short_cadence_observations):
                 observation_file_name = f'{kepler_input_catalog_number}_{observation_index}.fits'
                 try:
-                    observation.download().to_fits(os.path.join(data_directory, observation_file_name), overwrite=True)
+                    observation.download().to_fits(os.path.join(directory, observation_file_name), overwrite=True)
+                    observations_downloaded += 1
+                    if observations_downloaded >= max_observations:
+                        break
                 except OSError:
                     observations_skipped += 1
                     if not current_target_skipped:
                         current_target_skipped = True
                         targets_skipped += 1
+        if observations_downloaded >= max_observations:
+            break
     print(f'{targets_skipped} targets skipped.')
     print(f'{observations_skipped} observations skipped.')
+    print(f'{observations_downloaded} observations downloaded.')
+    return observations_downloaded
 
 
 if __name__ == '__main__':
-    star_list_ = get_easy_positive_stars()
-    print_short_cadence_observation_count_for_stars(star_list_)
+    positive_star_list = get_easy_positive_stars()
+    positive_observation_count = download_all_short_cadence_observations_for_star_list(positive_star_list,
+                                                                                       positive_data_directory)
+    negative_star_list = get_negative_stars()
+    download_all_short_cadence_observations_for_star_list(positive_star_list, negative_data_directory,
+                                                          max_observations=positive_observation_count)
+
+
