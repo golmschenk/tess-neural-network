ca1a08e0a9779088284bb4d4baa60a163e950ff0
Greg Olmschenk <golmschenk@gmail.com>
2019-03-10 18:43:34 -0400

    Disable gradient calculation during evaluation for speed purposes

--- koi_train.py
+++ koi_train.py
@@ -11,11 +11,11 @@ from koi_model import SmallNet
 
 def train():
     """Trains and evaluates the KOI network."""
-    batch_size = 50
-    training_dataset_size = 5000
-    train_dataset = KoiCatalogDataset(start=None, end=training_dataset_size)
+    batch_size = 100
+    validation_dataset_size = 2000
+    train_dataset = KoiCatalogDataset(start=None, end=-validation_dataset_size)
     train_dataset_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    validation_dataset = KoiCatalogDataset(start=training_dataset_size, end=None)
+    validation_dataset = KoiCatalogDataset(start=-validation_dataset_size, end=None)
     validation_dataset_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True)
 
     network = SmallNet()
@@ -33,12 +33,13 @@ def train():
             predicted_labels = torch.sigmoid(predicted_scores.detach()).round().numpy()
             total_train_incorrect += np.sum(predicted_labels != labels.detach().numpy())
         train_error = total_train_incorrect / len(train_dataset)
-        total_validation_incorrect = 0
-        for validation_examples, validation_labels in validation_dataset_loader:
-            predicted_validation_scores = network(validation_examples)
-            predicted_validation_labels = torch.sigmoid(predicted_validation_scores.detach()).round().numpy()
-            total_validation_incorrect += np.sum(predicted_validation_labels != validation_labels.detach().numpy())
-        validation_error = total_validation_incorrect / len(validation_dataset)
+        with torch.no_grad():  # For speed, don't calculate gradient during evaluation.
+            total_validation_incorrect = 0
+            for validation_examples, validation_labels in validation_dataset_loader:
+                predicted_validation_scores = network(validation_examples)
+                predicted_validation_labels = torch.sigmoid(predicted_validation_scores.detach()).round().numpy()
+                total_validation_incorrect += np.sum(predicted_validation_labels != validation_labels.detach().numpy())
+            validation_error = total_validation_incorrect / len(validation_dataset)
         print('Epoch: {}, Train error: {:.5f}, Validation error: {:.5f}'.format(epoch, train_error, validation_error))
 
 
